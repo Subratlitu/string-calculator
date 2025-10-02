fea2301f40c9226b89505253abb23e9d2a9cfc89
subrat <subratnayak940@gmail.com>
2025-10-02 11:35:26 +0530

    test: add failing test for newline as valid delimiter

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -16,3 +16,8 @@ test('any amount of numbers comma separated return their sum', () => {
   expect(add("1,2,3,4,5")).toBe(15);
 });
 
+test('numbers can be separated by newlines as well as commas', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+
